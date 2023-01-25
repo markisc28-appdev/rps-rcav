55863ca8458e4828344be1fd3e59fa0ac7ad11dc
markischengg <markischeng28@uchicago.edu>
2023-01-25 17:34:27 +0000

    rock page playing works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # redirect_to("https://firstdraft.slides.com/raghubetina/06-routing-rcav?token=43w7FD8Q#/12/0/4")
+
+    # render({ :html => "<h1>Hello, world!<h1>".html_safe})
+
+    render({ :template => "game_templates/user_rock.html.erb"})
+  end
+
 end
